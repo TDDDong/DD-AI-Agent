8b9d7b2881aa14d95e870dc65964594c7eef7129
12618 <1261801883@qq.com>
2025-04-27 20:00:07 +0800

    2025-04-27 1.初始化dd-ai-agent项目

---
+++ src/test/java/com/dd/ddaiagent/DdAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dd.ddaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
