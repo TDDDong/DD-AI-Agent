d7d153830ff5f321f4bd55070a2479cba32a7db4
12618 <1261801883@qq.com>
2025-05-07 23:20:19 +0800

    2025-05-07 1.采用PromptTemplate模板创建系统信息

--- src/main/java/com/dd/ddaiagent/app/LoveApp.java
+++ src/main/java/com/dd/ddaiagent/app/LoveApp.java
@@ -1,18 +1,21 @@
 package com.dd.ddaiagent.app;
 
 import com.dd.ddaiagent.advisor.MyLoggerAdvisor;
-import com.dd.ddaiagent.chatMemory.FileBasedChatMemory;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
@@ -23,16 +26,29 @@ public class LoveApp {
 
     public final ChatClient chatClient;
 
-    private static final String SYSTEM_PROMPT = "扮演深耕恋爱心理领域的专家。开场向用户表明身份，告知用户可倾诉恋爱难题。" +
+    // 从类路径资源加载系统提示模板
+    @Value("classpath:/prompts/system-message.st")
+    private Resource systemResource;
+
+    /*private static final String SYSTEM_PROMPT = "扮演深耕恋爱心理领域的专家。开场向用户表明身份，告知用户可倾诉恋爱难题。" +
             "围绕单身、恋爱、已婚三种状态提问：单身状态询问社交圈拓展及追求心仪对象的困扰；" +
             "恋爱状态询问沟通、习惯差异引发的矛盾；已婚状态询问家庭责任与亲属关系处理的问题。" +
-            "引导用户详述事情经过、对方反应及自身想法，以便给出专属解决方案。";
+            "引导用户详述事情经过、对方反应及自身想法，以便给出专属解决方案。";*/
 
 
-    public LoveApp(ChatModel dashScopeChatModel, @Qualifier("redisChatMemory") ChatMemory chatMemory) {
+
+    public LoveApp(ChatModel dashScopeChatModel, @Qualifier("redisChatMemory") ChatMemory chatMemory,
+                   @Value("classpath:/prompts/system-message.st") Resource systemResource) {
         //ChatMemory chatMemory = new InMemoryChatMemory();
         /*String fileDir = System.getProperty("user.dir") + "/tmp/chat-memory";
         ChatMemory chatMemory = new FileBasedChatMemory(fileDir);*/
+        // 直接使用资源创建模板
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(systemResource);
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("name", "恋爱心理");
+        variables.put("notice", "围绕单身、恋爱、已婚三种状态提问：单身状态询问社交圈拓展及追求心仪对象的困扰；" +
+                "恋爱状态询问沟通、习惯差异引发的矛盾；已婚状态询问家庭责任与亲属关系处理的问题。");
+        String SYSTEM_PROMPT = systemPromptTemplate.createMessage(variables).getText();
         chatClient = ChatClient.builder(dashScopeChatModel)
                 .defaultSystem(SYSTEM_PROMPT)
                 .defaultAdvisors(
@@ -70,7 +86,7 @@ public class LoveApp {
     public LoveReport doChatWithReport(String userMsg, String chatId) {
         LoveReport loveReport = chatClient.prompt()
                 .user(userMsg)
-                .system(SYSTEM_PROMPT + "每次对话后都要生成恋爱结果，标题为{用户名}的恋爱报告，内容为建议列表")
+                //.system(SYSTEM_PROMPT + "每次对话后都要生成恋爱结果，标题为{用户名}的恋爱报告，内容为建议列表")
                 .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
                         .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
                 .call()
